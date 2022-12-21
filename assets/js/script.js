7736cc7e2cc69d7bfa4a09e4703d6ae71ac650a5
LesterCuasay <lestercuasay@live.co.uk>
2022-12-21 13:11:37 +0000

    feat - added quiz question in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,82 @@
+const quizData = [
+    {
+        question: "What is the oldest recorded town in the UK",
+        a: ,
+        b: ,
+        c: ,
+        d: ,
+
+        correct: "" ,
+    },
+    {
+        question: "What is the highest mountain in Britian?",
+        a: ,
+        b: ,
+        c: ,
+        d: ,
+
+        correct: "" ,
+    },
+    {
+        question: "What is the capital of Canada",
+        a: ,
+        b: ,
+        c: ,
+        d: ,
+
+        correct: "" ,
+    },
+    {
+        question: "What is the largest desert in the world?",
+        a: ,
+        b: ,
+        c: ,
+        d: ,
+
+        correct: "" ,
+    },
+    {
+        question: "Which London Underground is reporesented as green on the tube map?",
+        a: ,
+        b: ,
+        c: ,
+        d: ,
+
+        correct: "" ,
+    },
+    {
+        question: "Which country has the most castles in the world?",
+        a: ,
+        b: ,
+        c: ,
+        d: ,
+
+        correct: "" ,
+    },
+    {
+        question: "How many large islands make up Hawaii?",
+        a: ,
+        b: ,
+        c: ,
+        d: ,
+
+        correct: "" ,
+    },
+    {
+        question: "Which river flows through Glasgow?",
+        a: ,
+        b: ,
+        c: ,
+        d: ,
+
+        correct: "" ,
+    },
+];
+
+const quiz = document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer')
+const questionEls = document.getElementById('question')
+const a_answer = document.getElementById('a_answer')
+const b_answer = document.getElementById('b_answer')
+const c_answer = document.getElementById('c_answer')
+const d_answer = document.getElementById('d_answer')
